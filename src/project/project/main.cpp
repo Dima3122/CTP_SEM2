0f207043c5fb43a62f796cba426aa1267aa0a71b
Dima3122 <dimasavostin89@gmail.com>
2022-02-05 12:34:29 +0700

    add test and add some changes

--- src/project/project/main.cpp
+++ src/project/project/main.cpp
@@ -2,14 +2,13 @@
 #include <Time.hpp>
 #include <TimeSpan.hpp>
 #include <unistd.h>
-#include <CustomLiterals.hpp>
 
 int main()
 {
     lab1::Time time1;
     sleep(5);
     lab1::Time time2;
-    lab1::TimeSpan spaner = time1 - time2;
+    lab1::TimeSpan spaner = time2 - time1;
     std::cout << time1 << std::endl;
     std::cout << std::endl;
     std::cout << time2 << std::endl;
@@ -18,8 +17,7 @@ int main()
     lab1::Time time3 = time2 + spaner;
     std::cout << std::endl;
     std::cout << time3 << std::endl;
-    lab1::Time time4;
-    time4 = time2 - spaner;
+    lab1::Time time4 = time2 - spaner;
     std::cout << std::endl;
     std::cout << time4 << std::endl;
     std::cout << std::endl;
--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,34 @@
 #include <Time.hpp>
 #include <gtest/gtest.h>
 
-TEST(test_case_name, test_name)
+TEST(test_Time, operator_subtraction)
 {
-	
-	ASSERT_EQ(5, 5) << "1 is not equal 0";
+	lab1::Time time1;
+    sleep(5);
+    lab1::Time time2;
+    lab1::TimeSpan spaner = time1 - time2;
+	time_t time = time2.get_now() - time1.get_now();
+	ASSERT_EQ(time, spaner.get_timeSpan());
+}
+
+TEST(test_Time, operator_sum)
+{
+	lab1::Time time1;
+    sleep(5);
+    lab1::Time time2;
+	lab1::TimeSpan spaner = time1 - time2;
+    lab1::Time time3 = time2 + spaner;
+	time_t time = time2.get_now() + spaner.get_timeSpan();
+	ASSERT_EQ(time, time3.get_now());
+}
+
+TEST(test_Time, operator_subtraction_other)
+{
+	lab1::Time time1;
+    sleep(5);
+    lab1::Time time2;
+	lab1::TimeSpan spaner = time1 - time2;
+    lab1::Time time3 = time2 - spaner;
+	time_t time = time2.get_now() - spaner.get_timeSpan();
+	ASSERT_EQ(time, time3.get_now());
 }
